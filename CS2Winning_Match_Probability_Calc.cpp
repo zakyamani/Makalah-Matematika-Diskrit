a518472a12128fcb9b7066a4b0f44d3ecbb47878
Zaky Amani <mzakyrice@gmail.com>
2026-06-17 16:35:00 +0700

    add OT switching side consideration and initial OT Money

--- CS2Winning_Match_Probability_Calc.cpp
+++ CS2Winning_Match_Probability_Calc.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 
 const int initMoney = 4000; // Initial money for the first round of the first half and second half (800 * 5 = 4000)
+const int initOTMoney = 50000; // Initial money for the first round of over time (Round 25 and Round 28)
 const int winBonus = 16250; // Additional money after a team win a round
 const int lossBonus = 12000; // Additional money after a team lose a round
-const int CTFullBuy = 22000; // Money that a CT team needed to full buy
-const int TFullBuy = 20000; // Money that a T team needed to full buy
+const int CTFullBuy = 24000; // Money that a CT team needed to full buy
+const int TFullBuy = 21000; // Money that a T team needed to full buy
 
 void teamInisiation(string *teamA, string *teamB, int *moneyA, int *moneyB, int *scoreA, int *scoreB){
     cout << "Insert the first team (First half playing as CT): ";
@@ -38,31 +39,41 @@ double calculate_win_probability(bool team, int moneyA, int moneyB, int scoreA, 
     if(((scoreB == 13) && (scoreA < 12)) || (scoreB == 16)){
         return !team;
     }
-    
-    if(scoreA + scoreB < 12){
+
+    if((scoreA + scoreB == 24) || (scoreA + scoreB == 27)){
+        moneyA = initOTMoney;
+        moneyB = initOTMoney;
+    }
+        
+    if((scoreA + scoreB < 12) || ((scoreA + scoreB >= 24) && (scoreA + scoreB < 27))){
         if((moneyA >= CTFullBuy) && (moneyB >= TFullBuy)){
-            return 0.5 * calculate_win_probability(team, moneyA - CTFullBuy + winBonus, moneyB - TFullBuy + lossBonus, scoreA + 1, scoreB) + 0.5 * calculate_win_probability(team, moneyA - CTFullBuy + lossBonus, moneyB - TFullBuy + winBonus, scoreA, scoreB + 1);
+            return 0.488 * calculate_win_probability(team, moneyA - (CTFullBuy/5)*3 + winBonus, moneyB - TFullBuy + lossBonus, scoreA + 1, scoreB) + 0.512 * calculate_win_probability(team, moneyA - CTFullBuy + lossBonus, moneyB - (TFullBuy/5)*3 + winBonus, scoreA, scoreB + 1);
         }else if((moneyA >= CTFullBuy) && (moneyB < TFullBuy)){
-            return 0.9 * calculate_win_probability(team, moneyA - CTFullBuy + winBonus, moneyB + lossBonus, scoreA + 1, scoreB) + 0.1 * calculate_win_probability(team, moneyA - CTFullBuy + lossBonus, moneyB + winBonus, scoreA, scoreB + 1);
+            return 0.96 * calculate_win_probability(team, moneyA - (CTFullBuy/5)*3 + winBonus, moneyB + lossBonus, scoreA + 1, scoreB) + 0.04 * calculate_win_probability(team, moneyA - CTFullBuy + lossBonus, moneyB + winBonus, scoreA, scoreB + 1);
         }else if((moneyA < CTFullBuy) && (moneyB >= TFullBuy)){
-            return 0.1 * calculate_win_probability(team, moneyA + winBonus, moneyB - TFullBuy + lossBonus, scoreA + 1, scoreB) + 0.9 * calculate_win_probability(team, moneyA + lossBonus, moneyB - TFullBuy + winBonus, scoreA, scoreB + 1);
+            return 0.04 * calculate_win_probability(team, moneyA + winBonus, moneyB - TFullBuy + lossBonus, scoreA + 1, scoreB) + 0.96 * calculate_win_probability(team, moneyA + lossBonus, moneyB - (TFullBuy/5)*3 + winBonus, scoreA, scoreB + 1);
         }else{
-            return 0.5 * calculate_win_probability(team, moneyA + winBonus, moneyB + lossBonus, scoreA + 1, scoreB) + 0.5 * calculate_win_probability(team, moneyA + lossBonus, moneyB  + winBonus, scoreA, scoreB + 1);
+            return 0.488 * calculate_win_probability(team, moneyA + winBonus, moneyB + lossBonus, scoreA + 1, scoreB) + 0.512 * calculate_win_probability(team, moneyA + lossBonus, moneyB  + winBonus, scoreA, scoreB + 1);
         }
     }else if(scoreA + scoreB == 12){ // at the second half, each team's money back to the initial money
         return 0.5 * calculate_win_probability(team, initMoney + winBonus, initMoney + lossBonus, scoreA + 1, scoreB) + 0.5 * calculate_win_probability(team, initMoney + lossBonus, initMoney + winBonus, scoreA, scoreB + 1);
     }else{
         if((moneyA >= TFullBuy) && (moneyB >= CTFullBuy)){
-            return 0.5 * calculate_win_probability(team, moneyA - TFullBuy + winBonus, moneyB - CTFullBuy + lossBonus, scoreA + 1, scoreB) + 0.5 * calculate_win_probability(team, moneyA - TFullBuy + lossBonus, moneyB - CTFullBuy + winBonus, scoreA, scoreB + 1);
+            return 0.512 * calculate_win_probability(team, moneyA - (TFullBuy/5)*3 + winBonus, moneyB - CTFullBuy + lossBonus, scoreA + 1, scoreB) + 0.488 * calculate_win_probability(team, moneyA - TFullBuy + lossBonus, moneyB - (CTFullBuy/5)*3 + winBonus, scoreA, scoreB + 1);
         }else if((moneyA >= TFullBuy) && (moneyB < CTFullBuy)){
-            return 0.9 * calculate_win_probability(team, moneyA - TFullBuy + winBonus, moneyB + lossBonus, scoreA + 1, scoreB) + 0.1 * calculate_win_probability(team, moneyA - TFullBuy + lossBonus, moneyB + winBonus, scoreA, scoreB + 1);
+            return 0.96 * calculate_win_probability(team, moneyA - (TFullBuy/5)*3 + winBonus, moneyB + lossBonus, scoreA + 1, scoreB) + 0.04 * calculate_win_probability(team, moneyA - TFullBuy + lossBonus, moneyB + winBonus, scoreA, scoreB + 1);
         }else if((moneyA < TFullBuy) && (moneyB >= CTFullBuy)){
-            return 0.1 * calculate_win_probability(team, moneyA + winBonus, moneyB - CTFullBuy + lossBonus, scoreA + 1, scoreB) + 0.9 * calculate_win_probability(team, moneyA + lossBonus, moneyB - CTFullBuy + winBonus, scoreA, scoreB + 1);
+            return 0.04 * calculate_win_probability(team, moneyA + winBonus, moneyB - CTFullBuy + lossBonus, scoreA + 1, scoreB) + 0.96 * calculate_win_probability(team, moneyA + lossBonus, moneyB - (CTFullBuy/5)*3 + winBonus, scoreA, scoreB + 1);
         }else{
-            return 0.5 * calculate_win_probability(team, moneyA + winBonus, moneyB + lossBonus, scoreA + 1, scoreB) + 0.5 * calculate_win_probability(team, moneyA + lossBonus, moneyB  + winBonus, scoreA, scoreB + 1);
+            return 0.512 * calculate_win_probability(team, moneyA + winBonus, moneyB + lossBonus, scoreA + 1, scoreB) + 0.488 * calculate_win_probability(team, moneyA + lossBonus, moneyB  + winBonus, scoreA, scoreB + 1);
         }
     }
+}
 
+void printCalculation(string teamA, string teamB, double AWin, double BWin){
+    cout << "The probability of " << teamA << " win the game is " << AWin << "%" << endl;
+    cout << "The probability of " << teamB << " win the game is " << BWin << "%" << endl;
+    cout << "The probability of the game end ties is " << 100 - AWin - BWin << "%";
 }
 
 int main(){
@@ -71,7 +82,5 @@ int main(){
     teamInisiation(&teamA,&teamB,&moneyA,&moneyB,&scoreA,&scoreB);
     double AWin = calculate_win_probability(1, moneyA, moneyB, scoreA, scoreB) * 100;
     double BWin = calculate_win_probability(0, moneyA, moneyB, scoreA, scoreB) * 100;
-    cout << "The probability of " << teamA << " win the game is " << AWin << "%" << endl;
-    cout << "The probability of " << teamB << " win the game is " << BWin << "%" << endl;
-    cout << "The probability of the game end ties is " << 100 - AWin - BWin << "%";
+    printCalculation(teamA, teamB, AWin, BWin);
 }
